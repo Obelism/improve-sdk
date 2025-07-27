f393b32d4c1b09e046ece1f0b49359c4fd88a9cd
Maarten Bruggink <maarten@obelism.studio>
2025-07-27 16:12:43 +0200

    🏗️ - Build scripts for examples

--- examples/react/src/index.tsx
+++ examples/react/src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { generateImproveProvider } from '@obelism/improve-sdk-react'
 
-const { ImproveProvider, useTestValue } = generateImproveProvider({
-	organizationId: 'org_MJFL46Z0WXGQ5OHW1ZXSM3Q88S',
-	environment: 'staging',
-})
+const { ImproveProvider, useTestValue, useImproveStatus } =
+	generateImproveProvider({
+		organizationId: 'org_MJFL46Z0WXGQ5OHW1ZXSM3Q88S',
+		environment: 'staging',
+	})
 
 const Example = () => {
+	const status = useImproveStatus()
 	const testValue = useTestValue('startpage-visual')
 
 	return (
